bf247e1dde2edb8602d8816bad1dd879ab615222
naoyuki <nfrutta1208@gmail.com>
2021-06-26 23:17:03 +0900

    時計表示するところまで

--- CopyPasteSnippet/CopyPasteSnippet/ViewModels/MainPageViewModel.cs
+++ CopyPasteSnippet/CopyPasteSnippet/ViewModels/MainPageViewModel.cs
@@ -1,10 +1,36 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Prism.Navigation;
+
 namespace CopyPasteSnippet.ViewModels
 {
-    public class MainPageViewModel
+    public class MainPageViewModel : ViewModelBase
     {
-        public MainPageViewModel()
+        private string _labelTime = string.Empty;
+        public string LabelTime
         {
+            get { return _labelTime; }
+            set
+            {
+                SetProperty(ref _labelTime, value);
+            }
+        }
+
+        public MainPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Title = "Main Page"; // 仮
+
+            // 時計
+            new Task(async () =>
+            {
+                while (true)
+                {
+                    Xamarin.Forms.Device.BeginInvokeOnMainThread(() =>
+                        LabelTime = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")
+                    );
+                    await Task.Delay(1000);
+                }
+            }).Start();
         }
     }
 }
