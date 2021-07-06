4cd8f84bcd622d136470f3546ea83335c526be26
naoyuki <nfrutta1208@gmail.com>
2021-07-06 21:53:13 +0900

    現在時刻出したところまで

--- SimpleTimecard/SimpleTimecard/ViewModels/MainPageViewModel.cs
+++ SimpleTimecard/SimpleTimecard/ViewModels/MainPageViewModel.cs
@@ -1,11 +1,42 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Prism.Navigation;
+using Xamarin.Forms;
 
 namespace SimpleTimecard.ViewModels
 {
-    public class MainPageViewModel
+    public class MainPageViewModel : ViewModelBase
     {
-        public MainPageViewModel()
+        /// <summary>
+        /// 現在時刻ラベルテキスト
+        /// </summary>
+        private string _labelCurrentTimeText = string.Empty;
+        public string LabelCurrentTimeText
         {
+            get
+            {
+                return _labelCurrentTimeText;
+            }
+            set
+            {
+                SetProperty(ref _labelCurrentTimeText, value);
+            }
+        }
+
+        public MainPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Title = "Top";
+
+            new Task(async () =>
+            {
+                while (true)
+                {
+                    Xamarin.Forms.Device.BeginInvokeOnMainThread(() =>
+                        LabelCurrentTimeText = DateTime.Now.ToString("yyyy/MM/dd ddd HH:mm:ss")
+                    );
+                    await Task.Delay(1000);
+                }
+            }).Start();
         }
     }
 }
---
+++ SimpleTimecard/SimpleTimecard/ViewModels/ViewModelBase.cs
@@ -0,0 +1,52 @@
+﻿using Prism.AppModel;
+using Prism.Mvvm;
+using Prism.Navigation;
+
+namespace SimpleTimecard.ViewModels
+{
+    public class ViewModelBase : BindableBase, INavigationAware, IDestructible, IPageLifecycleAware
+    {
+        /// <summary>
+        /// 画面タイトル
+        /// </summary>
+        private string _title;
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                SetProperty(ref _title, value);
+            }
+        }
+
+        protected INavigationService NavigationService
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="navigationService"></param>
+        public ViewModelBase(INavigationService navigationService)
+        {
+            NavigationService = navigationService;
+        }
+
+        public virtual void OnNavigatedFrom(INavigationParameters parameters) { }
+
+        public virtual void OnNavigatedTo(INavigationParameters parameters) { }
+
+        public virtual void OnNavigatingTo(INavigationParameters parameters) { }
+
+        public virtual void Destroy() { }
+
+        public virtual void OnAppearing() { }
+
+        public virtual void OnDisappearing() { }
+    }
+}
