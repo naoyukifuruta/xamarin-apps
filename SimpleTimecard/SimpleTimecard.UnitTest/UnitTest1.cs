1ac8b36503437e472c5611a9802b274f819a235b
naoyuki <nfrutta1208@gmail.com>
2021-07-20 21:32:59 +0900

    テストプロジェクトを追加

---
+++ SimpleTimecard/SimpleTimecard.UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleTimecard.ViewModels;
+
+namespace SimpleTimecard.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var vm = new TodayPageViewModel(null, null);
+            Assert.AreEqual("出勤時間：", vm.StartTimeLabelText);
+        }
+    }
+}
