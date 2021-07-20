0665a5da00c3de476abcee95b03044e67528306e
naoyuki <nfrutta1208@gmail.com>
2021-07-20 21:43:10 +0900

    メニューバーの設定ボタンのcommand追加

--- SimpleTimecard/SimpleTimecard/ViewModels/MainTabbedPageViewModel.cs
+++ SimpleTimecard/SimpleTimecard/ViewModels/MainTabbedPageViewModel.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using Prism.Commands;
 using Prism.Navigation;
 
 namespace SimpleTimecard.ViewModels
 {
     public class MainTabbedPageViewModel : ViewModelBase
     {
+        public DelegateCommand SettingButtonCommand { get; set; }
+
         public MainTabbedPageViewModel(INavigationService navigationService) : base(navigationService)
         {
+            SettingButtonCommand = new DelegateCommand(TransitionSettingPage);
+
             // ページタイトルに現在時刻を表示
             new Task(async () =>
             {
@@ -19,6 +25,11 @@ namespace SimpleTimecard.ViewModels
                     await Task.Delay(1000);
                 }
             }).Start();
+        }
+
+        private void TransitionSettingPage()
+        {
+            Debug.WriteLine("test");
         }
     }
 }
