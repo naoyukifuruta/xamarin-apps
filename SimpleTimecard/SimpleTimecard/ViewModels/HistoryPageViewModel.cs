6b67763b4034f093a9f1b4c43afe1f638dafd34b
naoyuki <nfrutta1208@gmail.com>
2021-07-28 22:55:08 +0900

    viewmodelリファクタリング

--- SimpleTimecard/SimpleTimecard/ViewModels/HistoryPageViewModel.cs
+++ SimpleTimecard/SimpleTimecard/ViewModels/HistoryPageViewModel.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Input;
-using Prism.Commands;
 using Prism.Navigation;
 using Realms;
 using SimpleTimecard.Models;
@@ -16,25 +14,12 @@ namespace SimpleTimecard.ViewModels
         public List<Timecard> TimecardHistories
         {
             get { return _timecardHistories; }
-            set
-            {
-                SetProperty(ref this._timecardHistories, value);
-            }
+            set { SetProperty(ref this._timecardHistories, value); }
         }
-
-        public DelegateCommand AddButtonCommand { get; set; }
-        public ICommand CellTappedCommand { get; set; }
 
         public HistoryPageViewModel(INavigationService navigationService) : base(navigationService)
         {
             Title = "History";
-
-            AddButtonCommand = new DelegateCommand(TransitionSettingPage);
-
-            CellTappedCommand = new Command<Timecard>((timecard) =>
-            {
-                TappedListCell(timecard);
-            });
         }
 
         public override void OnAppearing()
@@ -47,23 +32,20 @@ namespace SimpleTimecard.ViewModels
             TimecardHistories = allTimecards;
         }
 
-        private async void TransitionSettingPage()
+        public Command OnClickAdd => new Command(async () =>
         {
-            await NavigationService.NavigateAsync("AddPage");
-        }
+            await NavigationService.NavigateAsync(nameof(AddPage));
+        });
 
-        /// <summary>
-        /// 編集画面へ遷移
-        /// </summary>
-        /// <param name="timecard"></param>
-        private async void TappedListCell(Timecard timecard)
+        public Command OnClickListViewCell => new Command<Timecard>(async (timecard) =>
         {
             var param = new NavigationParameters();
             param.Add(nameof(Timecard), timecard);
 
             await NavigationService.NavigateAsync(nameof(EditPage), param);
-        }
+        });
 
+        // TODO: bindできるかも？あとで試す
         public void DeleteTimecard(string timecardId)
         {
             // 削除
