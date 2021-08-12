60ee9a8b02711bd8da6efc81c5bb46289bb6c907
naoyuki <nfrutta1208@gmail.com>
2021-08-12 18:05:39 +0900

    リファクタリング

--- SimpleTimecard/SimpleTimecard/ViewModels/TodayPageViewModel.cs
+++ SimpleTimecard/SimpleTimecard/ViewModels/TodayPageViewModel.cs
@@ -46,34 +46,16 @@ namespace SimpleTimecard.ViewModels
             Logger.Trace();
             base.OnAppearing();
 
-            // 当日分の取得
-            var realm = Realm.GetInstance();
-            var allTimecards = realm.All<Timecard>().ToList();
-
-            // TODO: ここの処理はあとで見直したい
-            if (allTimecards.Where(x => x.EntryDate.Value.ToLocalTime().ToString("yyyyMMdd") == DateTime.Now.ToString("yyyyMMdd")).Any())
-            {
-                var todayTimecard = allTimecards.Where(x => x.EntryDate.Value.ToLocalTime().ToString("yyyyMMdd") == DateTime.Now.ToString("yyyyMMdd")).First();
-                _todayTimecardId = todayTimecard.TimecardId;
-
-                if (!string.IsNullOrEmpty(todayTimecard.StartTimeString))
-                {
-                    StampingStartTimeLabelText = todayTimecard.StartTimeString;
-                }
-
-                if (!string.IsNullOrEmpty(todayTimecard.EndTimeString))
-                {
-                    StampingEndTimeLabelText = todayTimecard.EndTimeString;
-                }
-            }
-            else
-            {
-                _todayTimecardId = string.Empty;
-                StampingStartTimeLabelText = string.Empty;
-                StampingEndTimeLabelText = string.Empty;
-            }
+            // 今日分の表示
+            var todayTimecard = FetchTodayTimecard();
+            _todayTimecardId = todayTimecard.TimecardId;
+            StampingStartTimeLabelText = todayTimecard.StartTimeString;
+            StampingEndTimeLabelText = todayTimecard.EndTimeString;
         }
 
+        /// <summary>
+        /// 出勤登録
+        /// </summary>
         public Command OnClickStartTimeEntry => new Command(async () =>
         {
             var result = await _pageDialogService.DisplayAlertAsync("確認", "出勤時間の登録を行いますか？", "OK", "キャンセル");
@@ -82,38 +64,13 @@ namespace SimpleTimecard.ViewModels
                 return;
             }
 
-            // TODO: あとでリファクタリングする
-
-            var entryDateTime = DateTime.Now.ToLocalTime();
-            var realm = Realm.GetInstance();
-
-            if (string.IsNullOrEmpty(_todayTimecardId))
-            {
-                // 新規登録
-                realm.Write(() =>
-                {
-                    var addedTimecard = realm.Add<Timecard>(new Timecard()
-                    {
-                        EntryDate = entryDateTime,
-                        StartTimeString = entryDateTime.ToString("HH:mm"),
-                    });
-
-                    _todayTimecardId = addedTimecard.TimecardId;
-                });
-            }
-            else
-            {
-                // 更新
-                var timecard = realm.Find<Timecard>(_todayTimecardId);
-                realm.Write(() => {
-                    timecard.StartTimeString = entryDateTime.ToString("HH:mm");
-                    realm.Add<Timecard>(timecard, update: true);
-                });
-            }
-
-            StampingStartTimeLabelText = entryDateTime.ToString("HH:mm");
+            var timecard = RegisteredTimecard(RegistType.StartTime);
+            StampingStartTimeLabelText = timecard.StartTimeString;
         });
 
+        /// <summary>
+        /// 退勤登録
+        /// </summary>
         public Command OnClickEndTimeEntry => new Command(async () =>
         {
             var result = await _pageDialogService.DisplayAlertAsync("確認", "退勤時間の登録を行いますか？", "OK", "キャンセル");
@@ -122,19 +79,67 @@ namespace SimpleTimecard.ViewModels
                 return;
             }
 
+            var timecard = RegisteredTimecard(RegistType.EndTime);
+            StampingEndTimeLabelText = timecard.EndTimeString;
+        });
+
+        /// <summary>
+        /// 今日の出退勤時間を取得
+        /// </summary>
+        /// <returns>今日のタイムカード</returns>
+        private Timecard FetchTodayTimecard()
+        {
+            var realm = Realm.GetInstance();
+            var allTimecards = realm.All<Timecard>().ToList();
+
+            Func<Timecard, bool> predicate = timecard => timecard.EntryDate.Value.ToLocalTime().ToString("yyyyMMdd") == DateTime.Now.ToLocalTime().ToString("yyyyMMdd");
+
+            if (allTimecards.Where(predicate).Any())
+            {
+                return allTimecards.Where(predicate).First();
+            }
+
+            return new Timecard()
+            {
+                TimecardId = string.Empty,
+            };
+        }
+
+        private enum RegistType
+        {
+            StartTime = 0,
+            EndTime,
+        }
+
+        /// <summary>
+        /// 出退勤時間の登録
+        /// </summary>
+        /// <param name="registType">登録種別</param>
+        /// <returns>タイムカード</returns>
+        private Timecard RegisteredTimecard(RegistType registType)
+        {
             var entryDateTime = DateTime.Now.ToLocalTime();
             var realm = Realm.GetInstance();
 
             if (string.IsNullOrEmpty(_todayTimecardId))
             {
                 // 新規登録
+                var addTimecard = new Timecard()
+                {
+                    EntryDate = entryDateTime,
+                };
+                if (registType == RegistType.StartTime)
+                {
+                    addTimecard.StartTimeString = entryDateTime.ToString("HH:mm");
+                }
+                else
+                {
+                    addTimecard.EndTimeString = entryDateTime.ToString("HH:mm");
+                }
+
                 realm.Write(() =>
                 {
-                    var addedTimecard = realm.Add<Timecard>(new Timecard()
-                    {
-                        EntryDate = entryDateTime,
-                        EndTimeString = entryDateTime.ToString("HH:mm"),
-                    });
+                    var addedTimecard = realm.Add<Timecard>(addTimecard);
                     _todayTimecardId = addedTimecard.TimecardId;
                 });
             }
@@ -142,13 +147,24 @@ namespace SimpleTimecard.ViewModels
             {
                 // 更新
                 var timecard = realm.Find<Timecard>(_todayTimecardId);
-                realm.Write(() => {
-                    timecard.EndTimeString = entryDateTime.ToString("HH:mm");
-                    realm.Add<Timecard>(timecard, update: true);
-                });
+                if (registType == RegistType.StartTime)
+                {
+                    
+                    realm.Write(() => {
+                        timecard.StartTimeString = entryDateTime.ToString("HH:mm");
+                        realm.Add<Timecard>(timecard, update: true);
+                    });
+                }
+                else
+                {
+                    realm.Write(() => {
+                        timecard.EndTimeString = entryDateTime.ToString("HH:mm");
+                        realm.Add<Timecard>(timecard, update: true);
+                    });
+                }
             }
 
-            StampingEndTimeLabelText = entryDateTime.ToString("HH:mm");
-        });
+            return realm.Find<Timecard>(_todayTimecardId);
+        }
     }
 }
